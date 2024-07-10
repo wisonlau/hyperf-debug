a39d9e36921404d1cd5f840bd61948a90911e166
wisonlau <wisonlau@wisonlaudeMacBook-Pro.local>
2024-07-10 17:55:25 +0800

    特性: 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# hyperf-debug
-Debug for Hyperf
+# Hyperf debug
+>Debug for Hyperf
+
+## 安装组件:
+>composer require wisonlau/hyperf-debug
+
+## 安装 WebSocket 服务:
+>[详见 Hyperf 官方文档](https://doc.hyperf.io/#/zh/websocket-server)
+
+修改 config/autoload/server.php，增加以下配置。
+```php
+'servers' => [
+    [
+        'name' => 'ws',
+        'type' => Server::SERVER_WEBSOCKET,
+        'host' => '0.0.0.0',
+        'port' => 9502,
+        'sock_type' => SWOOLE_SOCK_TCP,
+        'callbacks' => [
+            Event::ON_HAND_SHAKE => [Hyperf\WebSocketServer\Server::class, 'onHandShake'],
+            Event::ON_MESSAGE => [Hyperf\WebSocketServer\Server::class, 'onMessage'],
+            Event::ON_CLOSE => [Hyperf\WebSocketServer\Server::class, 'onClose'],
+        ],
+    ],
+],
+```
+
+## 发布配置:
+>php bin/hyperf.php vendor:publish wisonlau/hyperf-debug
+
+## 注意事项：
+
+- 配置文件路径：config/autoload/hpdebug.php
+
+- 默认地址是 ws://127.0.0.1:9502, 需修改成服务实际配置地址
+
+## 使用方法：[composer.json](..%2F..%2Fyile%2Fdnmp%2Fwww%2Fapp-api%2Fcomposer.json)
+1. 通过自定义路由 Router 添加输出页面
+修改 /hyperf/config/routes.php，增加以下配置。
+```php
+Router::addRoute(['GET', 'POST', 'HEAD'], '/hpdebug', function() {
+    $wsdebug = new \Wisonlau\HyperfDebug\Hpdebug();
+    $response = new \Hyperf\HttpServer\Response();
+    return $response->raw($wsdebug->getHtml())->withHeader('content-type', 'text/html; charset=utf-8');
+});
+
+Router::addServer('ws', function () {
+    Router::get('/', Wisonlau\HyperfDebug\Hpdebug::class);
+});
+```
+
+2. Demo
+```php
+php>=8.0
+<?php
+namespace App\HttpController;
+
+use Hyperf\Di\Annotation\Inject;
+use Wisonlau\HyperfDebug\Hpdebug;
+
+class TestController 
+{
+    #[Inject]
+	protected Hpdebug $debug;
+
+	public function test()
+	{
+		$userData = [
+		    'uid' => 1,
+		    'username' => 'hyperf-debug',
+		];
+		$this->debug->send($userData);
+	}
+}
+
+php<8.0
+<?php
+namespace App\HttpController;
+
+use Hyperf\Di\Annotation\Inject;
+use Wisonlau\HyperfDebug\Hpdebug;
+
+class TestController 
+{
+   /**
+    * @Inject()
+    * @var Hpdebug
+    */
+	protected $debug;
+
+	public function test()
+	{
+		$userData = [
+		    'uid' => 1,
+		    'username' => 'hyperf-debug',
+		];
+		$this->debug->send($userData);
+	}
+}
+```
+
+3. 访问调试地址
+http://127.0.0.1:9501/wsdebug
+
+
+![debug.jpg](img/debug.jpg)
---
+++ src/Hpdebug.php
@@ -0,0 +1,190 @@
+<?php
+/**
+ * Debug for Hyperf
+ */
+
+namespace Wisonlau\HyperfDebug;
+
+use Hyperf\Contract\OnCloseInterface;
+use Hyperf\Contract\OnMessageInterface;
+use Hyperf\Contract\OnOpenInterface;
+use Hyperf\Utils\Str;
+use Swoole\Server;
+use Swoole\WebSocket\Server as WebSocketServer;
+
+
+/**
+ * Class Hpdebug
+ *
+ * @author  wison
+ * @version 1.0
+ */
+class Hpdebug implements OnMessageInterface, OnOpenInterface, OnCloseInterface
+{
+
+    /**
+     * @var \swoole_websocket_server
+     */
+    private $server;
+
+
+    /**
+     * @var string
+     */
+    private $host;
+
+
+    /**
+     * Hpdebug constructor.
+     */
+    public function __construct()
+    {
+        $config = config('hpdebug') ?? '';
+        $this->host = isset($config['debughost']) && $config['debughost'] ? $config['debughost'] : 'ws://127.0.0.1:9502';
+        $container = \Hyperf\Utils\ApplicationContext::getContainer();
+        $wsFactory = $container->get(\Hyperf\WebSocketServer\Server::class);
+        $this->server = $wsFactory->getServer();
+    }
+
+
+    /**
+     * @param WebSocketServer $server
+     * @param $frame
+     */
+    public function onMessage($server, $frame): void
+    {
+        $type = 'info';
+        if ($frame->data == 'pong') {
+            $type = 'pong';
+        }
+        $server->push($frame->fd, json_encode(['type' => $type, 'content' => $frame->data]));
+    }
+
+
+    /**
+     * @param Server $server
+     * @param int $fd
+     * @param int $reactorId
+     */
+    public function onClose($server, int $fd, int $reactorId): void
+    {
+        echo "ws server $fd closed\n";
+    }
+
+
+    /**
+     * @param WebSocketServer $server
+     * @param $request
+     */
+    public function onOpen($server, $request): void
+    {
+        $server->push($request->fd, json_encode(['type' => 'open']));
+    }
+
+
+    /**
+     * @param mixed $message
+     * @param string $type 用于前端标记
+     * @return bool
+     */
+    public function send($message, string $type = 'info')
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 4);
+        array_shift($trace);
+        $server = $this->server;
+        if( !empty( $server->connections ) ){
+            if(is_array($message)){
+                $content_type = 'Array';
+            }elseif(is_string($message) && Str::endsWith($message,'}',false)){
+                $content_type = 'Json';
+                $message = json_decode($message,true);
+            }elseif(is_string($message)){
+                $content_type = 'String';
+            }elseif(is_int($message)) {
+                $content_type = 'Int';
+            }elseif(is_float($message)){
+                $content_type = 'Float';
+            }elseif(is_object($message)){
+                $content_type = 'Object';
+                $message = $this->object2array( $message ); //兼容打印对象
+            }else{
+                $content_type = '未捕捉到的数据类型';
+            }
+            $jsonMessage = json_encode( [
+                'time'    => date( "Y-m-d H:i:s" ),
+                'type'    => $type,
+                'content_type'=>$content_type,
+                'content' => $message,
+                'debug_backtrace' => $trace,
+            ]);
+            foreach( $server->connections as $fd ){
+                $info = $server->connection_info( $fd );
+                if( isset( $info['websocket_status'] ) && $info['websocket_status'] === 3 ){
+
+                    $server->push( $fd, $jsonMessage );
+                }
+            }
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+
+    /**
+     * @param $object
+     * @return array
+     */
+    private function object2array($object)
+    {
+        if (is_object($object)) {
+            $object = (array)$object;
+        }
+        if (is_array($object)) {
+            foreach ($object as $key => $value) {
+                $object[$key] = $this->object2array($value);
+            }
+        }
+        return $object;
+    }
+
+
+    /**
+     * Convert encoding.
+     *
+     * @param array $array
+     * @param string $to_encoding
+     * @param string $from_encoding
+     *
+     * @return array
+     */
+    private function encoding($array, $to_encoding = 'UTF-8', $from_encoding = 'GBK'): array
+    {
+        $encoded = [];
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $encoded[$key] = $this->encoding($value, $to_encoding, $from_encoding);
+            } elseif (is_bool($value)) {
+                $encoded[$key] = $value;
+            } elseif (is_string($value) && mb_detect_encoding($value, 'UTF-8', true)) {
+                $encoded[$key] = $value;
+            } else {
+                $encoded[$key] = mb_convert_encoding($value, $to_encoding, $from_encoding);
+            }
+        }
+        return $encoded;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getHtml() : string
+    {
+        $file =  @file_get_contents(__DIR__ . '/../view/Debug.html');
+        $file = str_replace('{{debughost}}',$this->host, $file);
+        return $file;
+    }
+
+
+}
